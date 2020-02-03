573fbf19fa4011423727e73be0dd7eec26ac1dcf
Raphaël Barman <raphael.barman@epfl.ch>
2020-02-03 10:12:23 +0100

    pessimistic disconnect handling

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 SECRET_KEY = 'generateSecretKey'
 SQLALCHEMY_DATABASE_URI = 'mysql://test:1234@localhost/aucase?unix_socket=/run/mysqld/mysqld.sock'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ENGINE_OPTIONS = { 'pool_pre_ping': True }
 JSON_AS_ASCII = False
 TESTING = False
 DEBUG = False
